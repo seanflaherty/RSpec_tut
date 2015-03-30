98106041af2f297f8fb4e104c64ad589688a7978
Sean Flaherty <sean.flaherty@gmail.com>
2015-03-30 11:06:33 -0400

    Dealt with the Spec "Should" warnings by using expect instead.

--- spec/book_spec.rb
+++ spec/book_spec.rb
@@ -7,23 +7,25 @@ describe Book do
 
     describe "#new" do
         it "takes three parameters and returns a Book object" do
-            @book.should be_an_instance_of Book
+            expect(@book).to be_a Book 
         end
     end
 
     describe "#title" do
         it "returns the correct title" do
-            @book.title.should eql "Title"
+            @book.title { is_expected eql "Title" }
         end
     end
+
     describe "#author" do
         it "returns the correct author" do
-            @book.author.should eql "Author"
+            @book.author { is_expected eql "Author" }
         end
     end
+
     describe "#category" do
         it "returns the correct category" do
-            @book.category.should eql :category
+            @book.category { is_expected eql :category }
         end
     end
 end
